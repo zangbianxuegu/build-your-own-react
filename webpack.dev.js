68f65294198796acb0b49e130a2252850c813b6e
zangbianxuegu <zangbianxuegu@gmail.com>
2020-07-30 18:14:22 +0800

    update

---
+++ webpack.dev.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  devtool: 'eval-cheap-module-source-map',
+  entry: './src/index.js',
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, 'dist'),
+  },
+  node: {
+    fs: 'empty',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: true,
+    }),
+  ],
+}
